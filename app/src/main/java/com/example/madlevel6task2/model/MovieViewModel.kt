c728ca5ea2cb76b37ed6cb95e2cdd36223dd424d
Nina Lemmens <nina.lemmens@hva.nl>
2022-04-04 12:11:51 +0200

    MovieViewModel set up

--- app/src/main/java/com/example/madlevel6task2/model/MovieViewModel.kt
+++ app/src/main/java/com/example/madlevel6task2/model/MovieViewModel.kt
@@ -1,20 +1,47 @@
 package com.example.madlevel6task2.model
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.*
 import com.example.madlevel6task2.repository.MovieRepository
+import kotlinx.coroutines.launch
 
-class MovieViewModel : ViewModel(){
-//
-//    private val movieRepository = MovieRepository()
-//
-//    //use encapsulation to expose as LiveData
-//    val movieItems: LiveData<List<MovieItem>>
-//    get() = _movieItems
-//
-//    private val _movieItems = MutableLiveData<List<MovieItem>>().apply {
-//        value = movieRepository.getMovieItems()
-//    }
+class MovieViewModel(application: Application) : AndroidViewModel(application){
+
+    private val movieRepository = MovieRepository()
+
+    /**
+     * This property points direct to the LiveData in the repository, that value
+     * gets updated when the use clicks the submit button.
+     */
+    val movies = movieRepository.movies
+
+    private val _errorText: MutableLiveData<String> = MutableLiveData()
+
+    /**
+     * Expose non MutableLiveData via getter
+     * errortext can be observed from the Activity for error showing
+     */
+    val errorText: LiveData<String>
+    get() = _errorText
+
+    /**The viewModelScope is bound to Dispatchers.Main and will automatically be cancelled when the
+     * ViewModel is cleared.
+     * Extension method of lifecycle-viewmodel-ktx library
+     */
+    fun getMoviesForYear(year: Int) {
+        viewModelScope.launch {
+            try {
+                //The movieRepository sets its own livedata property
+                //our own movies property points to this one
+                movieRepository.getMoviesForYear(year)
+            } catch (error: MovieRepository.MovieError) {
+                _errorText.value = error.message
+                Log.e("Movie error", error.cause.toString())
+            }
+
+        }
+    }
+
 
 }
